367ddeb5d4e901b45ba1320ffd1a948990828adf
dnyaneshwar <9dnyanesh@gmail.com>
2023-03-05 15:03:12 +0530

    single-level-inhertance programme

---
+++ single-level-inhetance.java
@@ -0,0 +1,99 @@
+// Java program to illustrate the
+// concept of single inheritance
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Bicycle {
+	
+	public int gear;
+	public int speed;
+
+
+	public Bicycle(int gear, int speed)
+	{
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+	
+	public void applyBrake(int decrement)
+	{
+		speed -= decrement;
+	}
+
+	public void speedUp(int increment)
+	{
+		speed += increment;
+	}
+
+
+	public String toString()
+	{
+		return ("No of gears are " + gear + "\n"
+				+ "speed of bicycle is " + speed);
+	}
+}
+
+
+class MountainBike extends Bicycle {
+
+	// the MountainBike subclass adds one more field
+	public int seatHeight;
+
+	// the MountainBike subclass has one constructor
+	public MountainBike(int gear, int speed,
+						int startHeight)
+	{
+		// invoking base-class(Bicycle) constructor
+		super(gear, speed);
+		seatHeight = startHeight;
+	}
+
+	// the MountainBike subclass adds one more method
+	public void setHeight(int newValue)
+	{
+		seatHeight = newValue;
+	}
+
+	// overriding toString() method
+	// of Bicycle to print more info
+	@Override public String toString()
+	{
+		return (super.toString() + "\nseat height is "
+				+ seatHeight);
+	}
+}
+
+// driver class
+public class Test {
+	public static void main(String args[])
+	{
+
+		MountainBike mb = new MountainBike(3, 100, 25);
+		System.out.println(mb.toString());
+	}
+}
+
+
+
+class one {
+	public void print_geek()
+	{
+		System.out.println("Geeks");
+	}
+}
+
+class two extends one {
+	public void print_for() { System.out.println("for"); }
+}
+// Driver class
+public class Main {
+	public static void main(String[] args)
+	{
+		two g = new two();
+		g.print_geek();
+		g.print_for();
+		g.print_geek();
+	}
+}
+
